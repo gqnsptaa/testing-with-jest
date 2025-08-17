e2591f6052f216702d95806beac04bdca6d0d774
Artem Blinkov <blinus677@gmail.com>
2025-08-17 10:25:42 +0200

    Del 2: setup webpack+jest, stack module, failing test -> fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,13 @@
 const path = require('path');
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
-    },
-    plugins: [
-        new CopyWebpackPlugin({
-            patterns: [{
-                context: __dirname + '/src/',
-                from: '*.html'
-            }]
-        })
-    ]
+  entry: './src/index.js',
+  output: { path: path.resolve(__dirname, 'dist'), filename: 'main.js' },
+  plugins: [
+    new CopyWebpackPlugin({ patterns: [{ context: path.resolve(__dirname, 'src'), from: '*.html' }] })
+  ],
+  mode: 'production'
 };
+
 
